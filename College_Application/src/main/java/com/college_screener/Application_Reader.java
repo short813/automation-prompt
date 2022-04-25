57bcb21db663039853b555c2e7e8afc1df07872d
short813 <short643@gmail.com>
2022-04-25 09:14:07 -0700

    Create method to store the candidate information

--- College_Application/src/main/java/com/college_screener/Application_Reader.java
+++ College_Application/src/main/java/com/college_screener/Application_Reader.java
@@ -1,5 +1,8 @@
 package com.college_screener;
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Hashtable;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
@@ -11,17 +14,14 @@ public class Application_Reader {
     public String lastName;
     public String state;
     public int felonies;
-    public int SATScore = 0;
-    public int ACTScore = 0;
     public String gpa;
-    public String felonyDate = "00-00-0000";
     public String age;
     public JSONObject application;
     public Application_Reader() {
     }
 
 
-    public void Candidate_Information(JSONObject application) throws IOException, ParseException {
+    public void Get_Candidate_Information(JSONObject application) throws IOException, ParseException {
         this.application = application;
         firstName = application.get("FirstName").toString();
         lastName = application.get("LastName").toString();
@@ -31,7 +31,39 @@ public class Application_Reader {
         felonies = Integer.parseInt(application.get("Felonies").toString());
     }
 
-    public void Name_Validator(){
+    public HashMap Store_Candidate_Information(){
+        HashMap<String,Object> candidate = new HashMap<>();
+        candidate.put("FirstName",firstName);
+        candidate.put("LastName",lastName);
+        candidate.put("State",state);
+        candidate.put("GPA",gpa);
+        candidate.put("Age",age);
+        candidate.put("Felonies",felonies);
 
+        if(application.get("ACT") == null) {
+            candidate.put("ACT", null);
+        }
+        else{
+            Long act = (Long) application.get("ACT");
+            candidate.put("ACT",act);
+        }
+        if(application.get("SAT") == null) {
+            candidate.put("SAT", null);
+        }
+        else{
+            Long sat = (Long) application.get("SAT");
+            candidate.put("SAT",sat);
+
+        }
+        if(application.get("FelonyDate") == null) {
+            candidate.put("FelonyDate", null);
+        }
+        else{
+            Date felonyDate = (Date) application.get("Felony_Date");
+            candidate.put("FelonyDate",felonyDate);
+        }
+        System.out.println(candidate);
+
+    return candidate;
     }
 }
--- College_Application/src/main/java/com/college_screener/Main.java
+++ College_Application/src/main/java/com/college_screener/Main.java
@@ -5,18 +5,24 @@ import org.json.simple.parser.ParseException;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
-        Application_Import();
+        Application_Processor();
 
     }
 
-    public static void Application_Import() throws IOException, ParseException {
+    public static void Application_Processor() throws IOException, ParseException {
         Object application = new JSONParser().parse(new FileReader("src/main/resources/John_Smith_application.json"));
         JSONObject application_json = (JSONObject) application;
         Application_Reader result = new Application_Reader();
-        result.Candidate_Information(application_json);
+        result.Get_Candidate_Information(application_json);
+        HashMap candidate_information = result.Store_Candidate_Information();
+        Application_Screener screen_results = new Application_Screener();
+        String results = screen_results.Screener(candidate_information);
+
+
 
     }
 }
